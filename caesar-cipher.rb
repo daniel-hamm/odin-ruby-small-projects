74a79941bafafdf0a226d3aaa6432d07c7636b45
Daniel Hamm <7501287+daniel-hamm@users.noreply.github.com>
2023-01-18 09:40:30 +0100

    Improve with ASCII conversion and clean up code

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,154 +1,50 @@
-
-# get text and the cipher key from opening arguments ARGV
-# open with args: ruby caesar-cipher.rb 'My text is awesome!' 3
-text = ARGV[0]
-key = ARGV[1]
-
-def convert_letter_to_number(letter)
-    case letter
-    when "a"
-        return 1
-    when "b"
-        return 2
-    when "c"
-        return 3
-    when "d"
-        return 4
-    when "e"
-        return 5
-    when "f"
-        return 6
-    when "g"
-        return 7
-    when "h"
-        return 8
-    when "i"
-        return 9
-    when "j"
-        return 10
-    when "k"
-        return 11
-    when "l"
-        return 12
-    when "m"
-        return 13
-    when "n"
-        return 14
-    when "o"
-        return 15
-    when "p"
-        return 16
-    when "q"
-        return 17
-    when "r"
-        return 18
-    when "s"
-        return 19
-    when "t"
-        return 20
-    when "u"
-        return 21
-    when "v"
-        return 22
-    when "w"
-        return 23
-    when "x"
-        return 24
-    when "y"
-        return 25
-    when "z"
-        return 26
-    end
-end
-
-def convert_number_to_text(number)
-    case number
-    when 1
-        return "a"
-    when 2
-        return "b"
-    when 3
-        return "c"
-    when 4
-        return "d"
-    when 5
-        return "e"
-    when 6
-        return "f"
-    when 7
-        return "g"
-    when 8
-        return "h"
-    when 9
-        return "i"
-    when 10
-        return "j"
-    when 11
-        return "k"
-    when 12
-        return "l"
-    when 13
-        return "m"
-    when 14
-        return "n"
-    when 15
-        return "o"
-    when 16
-        return "p"
-    when 17
-        return "q"
-    when 18
-        return "r"
-    when 19
-        return "s"
-    when 20
-        return "t"
-    when 21
-        return "u"
-    when 22
-        return "v"
-    when 23
-        return "w"
-    when 24
-        return "x"
-    when 25
-        return "y"
-    when 26
-        return "z"
-    end
-end
-
-def convert(text, key)
-
-    # downcase everything
-    text = text.downcase
-
+# method for converting a string to an array of numbers
+def convert_text_to_ascii(text)
+    
     # split the string to an array
     text = text.split("")
 
-    # convert the single letters inside the array to numbers; a = 1, b = 2 etc.
+    # convert the single letters inside the array to numbers; a = 97, b = 98, A = 65, B = 66 etc. according to the ASCII Table
     text.each_with_index do |value, index|
 
         # only convert to number if the letter is in the alphabet, no special characters or numbers
         if text[index].match?(/[A-Za-z]/)
-            text[index] = convert_letter_to_number(value)
+
+            # return the integer ordinal of our value (ASCII)
+            text[index] = value.ord
+
         end
 
     end
 
-    # convert the numbers inside the array to the given cipher key
+    # return text as array of ASCII numbers
+    text
+
+end
+
+# method for adding the cipher key to the array of ASCII numbers
+def add_cipher_key(text, key)
+
+    # convert the numbers inside the array according to the given cipher key
     text.each_with_index do |value, index|
 
         # only convert if the value is an integer, no special characters
         if text[index].class == Integer
 
             # calculate the new value; if the key is 3: z = 26 + 3 = 29
-            new_value = value + key.to_i
+            new_value = value + key
 
-            # if the value is above 26, start at 1 respectively the letter a
-            # 29 => 29 - 26 = 3 => letter: c
-            # else just return the new value
-            if new_value > 26
+            # function to substract 26 from the new number values if they are above a certain number to stay inside the alphabet
+
+            # if the ASCII value is between 97 and 122, we have downcase letters
+            # due to adding the cipher key, the value can be large than 122. if so: substract 26, because the alphabet has 26 letters and we want to stay inside the alphabet
+            # lets say we have a "z" as letter, which is 122 als ASCII value. if we add a key of 1, the new value is 123. So: 123 - 26 = 97. 97 to ASCII is "a"
+
+            # if the ASCII value is between 65 and 90, we have uppercase letters
+            # due to adding the cipher key, the value can be large than 90. if so: substract 26, because the alphabet has 26 letters and we want to stay inside the alphabet
+            # lets say we have a "Z" as letter, which is 90 als ASCII value. if we add a key of 1, the new value is 91. So: 91 - 26 = 65. 65 to ASCII is "A"
+
+            if (value >= 97 and value <= 122 and new_value > 122) or (value >= 65 and value <= 90 and new_value > 90)
                 text[index] = new_value - 26
             else
                 text[index] = new_value
@@ -158,18 +54,56 @@ def convert(text, key)
 
     end
 
+    # return text as array of ASCII numbers with added cipher key
+    text
+
+end
+
+def convert_ascii_to_number(text)
+
     # convert the single numbers inside the array to letters; 1 = a, 2 = b etc.
     text.each_with_index do |value, index|
 
         # only convert if the value is an integer, no special characters
         if text[index].class == Integer
-            text[index] = convert_number_to_text(value)
+
+            # convert the ASCII numbers to letters
+            text[index] = value.chr
+
         end
 
     end
 
-    puts text.join("")
+    # return text as array of letters
+    text
 
 end
 
-convert(text, key)
+def convert()
+
+    # get text and the cipher key from opening arguments ARGV
+    # open with args: ruby caesar-cipher.rb 'My text is awesome!' 3
+    text = ARGV[0]
+    key = ARGV[1].to_i
+
+    # add the modulo operator to the key
+    # keys above 26 don't make sense, since we only have 26 letters
+    # lets say we have 27 % 26 = 1; the key starts at 1 again
+    key = key % 26
+
+    # call the method to convert the given text to an array of ASCII numbers
+    text = convert_text_to_ascii(text)
+
+    # call the method to add the cipher key to the array of ASCII numbers
+    text = add_cipher_key(text, key)
+
+    # call the method to convert the array of ASCII numbers back to an array of letters / strings
+    text = convert_ascii_to_number(text)
+
+    # return the array of letters / strings and join back to a single string
+    text.join("")
+
+end
+
+# call the convert function
+puts convert()
